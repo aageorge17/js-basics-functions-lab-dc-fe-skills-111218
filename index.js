0969ac87d9382d240d8183aec6582503672d5071
Andrea George <aageorge17@gmail.com>
2019-01-23 23:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function distanceFromHqInFeet (blocks) {
 function distanceTravelledInFeet (startBlock, endBlock) {
   return Math.abs(startBlock - endBlock) * 264;
 }
+
+function calculatesFarePrice (startBlock, endBlock) {
+  if distanceTravelledInFeet(startBlock, endBlock)
+  return x
+}
