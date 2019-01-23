f0d81bda8e2ad541c20e2e764f8e03a55abf3d6e
Andrea George <aageorge17@gmail.com>
2019-01-23 23:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(startingblock) {
-  return Math.abs(42-'startingblock');
+function distanceFromHqInBlocks(blocks) {
+  return Math.abs(42-'blocks');
 }
