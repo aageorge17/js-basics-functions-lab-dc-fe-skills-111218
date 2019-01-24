bcd28e1c95a968ab6746bbf6bcc32e4cad1cb973
Andrea George <aageorge17@gmail.com>
2019-01-24 00:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ function distanceTravelledInFeet (startBlock, endBlock) {
 }
 
 function calculatesFarePrice (startBlock, endBlock) {
+   if (distanceTravelledInFeet(startBlock, endBlock)<400) {
+     return 0;
+   }
    if (distanceTravelledInFeet(startBlock, endBlock)> 2500) {
     return 'cannot travel that far'; }
   if (distanceTravelledInFeet(startBlock, endBlock)> 2000) {
     return 25;
   }
-  
   if (distanceTravelledInFeet(startBlock, endBlock)> 400) {
-    return distanceTravelledInFeet(startBlock, endBlock) *.02;
+    return distanceTravelledInFeet(startBlock, endBlock) *(.02);
   }
 }
